7cb0af39d512224062e98f63fd642cc8b1576219
Harshithapofficial <harshithaofficial26@gmail.com>
2026-03-11 22:13:50 +0530

    Product.java

--- src/Product.java
+++ src/Product.java
@@ -5,13 +5,17 @@ public class Product {
     double price;
 
     public Product(int id, String name, double price){
+
         this.id = id;
         this.name = name;
         this.price = price;
+
     }
 
     public void display(){
-        System.out.println(id + " " + name + " Rs." + price);
+
+        System.out.println(id + " | " + name + " | Rs." + price);
+
     }
 
 }
