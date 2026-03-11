0daba4cb745209497de3f5450af6df75e87c3c7f
Harshithapofficial <harshithaofficial26@gmail.com>
2026-03-11 22:19:03 +0530

    Create Payment.java

---
+++ src/Payment.java
@@ -0,0 +1,11 @@
+public class Payment {
+
+    public void processPayment(double amount){
+
+        System.out.println("Processing payment...");
+        System.out.println("Amount Paid: Rs." + amount);
+        System.out.println("Payment Successful");
+
+    }
+
+}
