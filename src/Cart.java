16163fd678520d6eef513e04ef93765d1149751f
Harshithapofficial <harshithaofficial26@gmail.com>
2026-03-11 22:14:20 +0530

    Cart.java

--- src/Cart.java
+++ src/Cart.java
@@ -5,14 +5,39 @@ public class Cart {
     ArrayList<Product> items = new ArrayList<>();
 
     public void addProduct(Product p){
+
         items.add(p);
         System.out.println("Product added to cart");
+
     }
 
     public void showCart(){
-        for(Product p : items){
-            p.display();
+
+        if(items.size()==0){
+            System.out.println("Cart is empty");
+            return;
         }
+
+        for(Product p : items){
+
+            p.display();
+
+        }
+
+    }
+
+    public double getTotal(){
+
+        double total = 0;
+
+        for(Product p : items){
+
+            total += p.price;
+
+        }
+
+        return total;
+
     }
 
 }
